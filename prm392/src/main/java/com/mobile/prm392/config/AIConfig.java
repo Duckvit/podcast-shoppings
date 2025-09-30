438eecdc9368853caa704a2e36ba350a7507bdcf
nguyenduyanh <anhndse182955@fpt.edu.vn>
2025-09-30 12:58:33 +0700

    version 7( use env for api key google, gmail)

--- prm392/src/main/java/com/mobile/prm392/config/AIConfig.java
+++ prm392/src/main/java/com/mobile/prm392/config/AIConfig.java
@@ -7,19 +7,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AIConfig {
+    @Bean
+    public Client AIClient() {
+        String key = System.getenv("GOOGLE_API_KEY");
+        System.out.println("GOOGLE_API_KEY = " + key); // kiểm tra giá trị
+        return Client.builder()
+                .apiKey(System.getenv("GOOGLE_API_KEY"))
+                .build();
+    }
+//@Value("${spring.ai.gemini.api-key}")
+//private String apiKey;
+//
 //    @Bean
 //    public Client AIClient() {
 //        return Client.builder()
-//                .apiKey(System.getenv("GOOGLE_API_KEY"))
+//                .apiKey(apiKey)  // lấy từ application.properties
 //                .build();
 //    }
-@Value("${spring.ai.gemini.api-key}")
-private String apiKey;
-
-    @Bean
-    public Client AIClient() {
-        return Client.builder()
-                .apiKey(apiKey)  // lấy từ application.properties
-                .build();
-    }
 }
